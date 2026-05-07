d3971ad168182dd7f635c73fd527177d451965be
Suxumuxu <emmanouil.xylakis@gmail.com>
2026-05-07 10:33:17 +0300

    latest

---
+++ OtherFuncs.cs
@@ -0,0 +1,123 @@
+﻿using System;
+
+
+namespace ConsoleApp1
+{ 
+
+	internal class OtherFuncs
+	{
+        //string playerInput = Console.ReadLine();
+
+        //Console.WriteLine("Your input was: " + playerInput);
+
+        //Console.WriteLine(format: "Value is {0} and {1}.", arg0: 19.8, arg1:65);
+
+        //InputHandler inputHandler = new InputHandler();
+
+        //inputHandler.getNameFromConsole();
+        //inputHandler.getCompleteSheetFromConsole();
+
+        //simpleHelloWorld();
+
+        //declarePlayerVariables();
+
+        //testMultipleNamespaces();
+
+        //makeABeep();
+        //Console.Write("What is the size of the maps ");
+        //string mapSize = Console.ReadLine();
+
+        static void simpleHelloWorld()
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("This is another test");
+            Console.Write("My name");
+            Console.Write("is");
+            Console.Write("Manolis");
+            Console.WriteLine('\n');
+        }
+
+        static void declarePlayerVariables()
+        {
+            int playerScore = 115, bonusScore = 55;
+            float scoreModifier, distanceToTarget = 105.6f, playerHeight = 175;
+            string playerName = "The One and Only, the Mighty Steve", inventoryItem = "Purple gem", latestFoundTreasure = "Steel sword";
+            bool playerIsDead = true;
+
+            Console.WriteLine("High score:" + playerIsDead + ", this is quite high!");
+
+            Console.WriteLine($"High score: {playerIsDead} this is quite high!");
+
+            Console.WriteLine($"Distance to target is: {distanceToTarget}");
+            Console.WriteLine($"{playerName} has found {latestFoundTreasure} and has aquired from the Dragon's Den {inventoryItem}.");
+            Console.Beep();
+
+            int finalScore = playerScore + bonusScore;
+            Console.WriteLine("Final score: " + finalScore);
+
+            int numberOfEnemies;
+            numberOfEnemies = 10;
+            Console.WriteLine("There are " + numberOfEnemies + " enemies.");
+
+        }
+
+        static void testMultipleNamespaces()
+        {
+            StudioA.Calculator.Message();
+
+            StudioB.Calculator.Message();
+        }
+
+        static void makeABeep()
+        {
+
+            Console.WriteLine("Press any key to stop...");
+
+            while (!Console.KeyAvailable)
+            {
+                Console.Beep();      // default beep
+                Thread.Sleep(10);  // small delay so it's not insane
+            }
+
+            // Consume the key so it doesn't stay in buffer
+            Console.ReadKey(true);
+
+            Console.WriteLine("Stopped.");
+        }
+
+
+
+
+    }
+
+
+
+
+
+
+}
+
+
+namespace StudioA
+{
+
+    internal class Calculator
+    {
+        // Προσθέστε τη λέξη static για να μπορεί να κληθεί άμεσα
+        public static void Message()
+        {
+            Console.WriteLine("This class belongs to namespace StudioA.");
+        }
+    }
+}
+namespace StudioB
+{
+    class Calculator
+    {
+        // Προσθέστε τη λέξη static εδώ επίσης
+        public static void Message()
+        {
+            Console.WriteLine("This class belongs to namespace StudioB.");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,44 +10,27 @@
         static public int playerPosX = (int)mapSize/2;
         static public int playerPosY = (int)mapSize/2;
 
-        static public int playerHealth = 85;
+        static public int playerHealth = 100;
         static public string dungeonName = "The swamps";
         static public int dungeonLevel = 1;
         static public int playerExp = 0;
         static public string playerName = " "; 
 
 
-
         static void Main(string[] args)
         {
 
-            //string playerInput = Console.ReadLine();
-
-            //Console.WriteLine("Your input was: " + playerInput);
-
-            //Console.WriteLine(format: "Value is {0} and {1}.", arg0: 19.8, arg1:65);
-
-            //InputHandler inputHandler = new InputHandler();
-
-            //inputHandler.getNameFromConsole();
-            //inputHandler.getCompleteSheetFromConsole();
-
-            //simpleHelloWorld();
-
-            //declarePlayerVariables();
-
-            //testMultipleNamespaces();
-
-            //makeABeep();
-            //Console.Write("What is the size of the maps ");
-            //string mapSize = Console.ReadLine();
+            
 
             char[,]  myMap = createAMap(mapSize);
 
-            myMap = updateMyMap(myMap);
+            // 1st run of my Map
+            myMap = updateMyMapCharMovement(myMap);
             drawMyMap(myMap);
             drawMyStats();
 
+
+            // This runs in loop 
             makeAMove(myMap);
             
 
@@ -182,65 +165,6 @@
             
         }
 
-        static void simpleHelloWorld()
-        {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("This is another test");
-            Console.Write("My name");
-            Console.Write("is");
-            Console.Write("Manolis");
-            Console.WriteLine('\n');
-        }
-
-        static void declarePlayerVariables()
-        {
-            int playerScore = 115, bonusScore = 55;
-            float scoreModifier, distanceToTarget = 105.6f, playerHeight = 175;
-            string playerName = "The One and Only, the Mighty Steve", inventoryItem = "Purple gem", latestFoundTreasure = "Steel sword";
-            bool playerIsDead = true;
-
-            Console.WriteLine("High score:" + playerIsDead + ", this is quite high!");
-
-            Console.WriteLine($"High score: {playerIsDead} this is quite high!");
-
-            Console.WriteLine($"Distance to target is: {distanceToTarget}");
-            Console.WriteLine($"{playerName} has found {latestFoundTreasure} and has aquired from the Dragon's Den {inventoryItem}.");
-            Console.Beep();
-            
-            int finalScore = playerScore + bonusScore;
-            Console.WriteLine("Final score: " + finalScore);
-
-            int numberOfEnemies;
-            numberOfEnemies = 10;
-            Console.WriteLine("There are " + numberOfEnemies + " enemies.");
-
-        }
-
-        static void testMultipleNamespaces()
-        {
-            StudioA.Calculator.Message();
-
-            StudioB.Calculator.Message();
-        }
-   
-        static void makeABeep()
-        {
-
-            Console.WriteLine("Press any key to stop...");
-
-            while (!Console.KeyAvailable)
-            {
-                Console.Beep();      // default beep
-                Thread.Sleep(10);  // small delay so it's not insane
-            }
-
-            // Consume the key so it doesn't stay in buffer
-            Console.ReadKey(true);
-
-            Console.WriteLine("Stopped.");
-        }
-
-
         static void makeAMove(char[,] myMap)
         {
             Console.WriteLine("Use arrow keys (Esc to quit):");
@@ -252,13 +176,16 @@
                 switch (key.Key)
                 {
                     case ConsoleKey.UpArrow:
+                        
                         Console.Clear();
                         playerPosY -= 1;
+                        
+                        //check if we are out of bounds Y NORTH 
                         if (playerPosY < 0)
                         {
                             Console.WriteLine("You cannot go there!");
                             playerPosY += 1;
-                            myMap = updateMyMap(myMap);
+                            myMap = updateMyMapCharMovement(myMap);
                             drawMyMap(myMap);
                             drawMyStats();
                             break;
@@ -266,18 +193,20 @@
                         else
                         {
                             Console.WriteLine("You go North");
-                            myMap = updateMyMap(myMap);
+                            myMap = updateMyMapCharMovement(myMap);
                             drawMyMap(myMap);
                             drawMyStats();
                             break;
                         }
                         
-
                     case ConsoleKey.DownArrow:
+                        
                         Console.Clear();
+                        //playerPosY += 1;
+
                         Console.WriteLine("You go South");
                         playerPosY += 1;
-                        myMap = updateMyMap(myMap);
+                        myMap = updateMyMapCharMovement(myMap);
                         drawMyMap(myMap);
                         drawMyStats();
                         break;
@@ -286,7 +215,7 @@
                         Console.Clear();
                         Console.WriteLine("You go West");
                         playerPosX -= 1;
-                        myMap = updateMyMap(myMap);
+                        myMap = updateMyMapCharMovement(myMap);
                         drawMyMap(myMap);
                         drawMyStats();
                         break;
@@ -295,7 +224,7 @@
                         Console.Clear();
                         Console.WriteLine("You go East");
                         playerPosX += 1;
-                        myMap = updateMyMap(myMap);
+                        myMap = updateMyMapCharMovement(myMap);
                         drawMyMap(myMap);
                         drawMyStats();
                         break;
@@ -361,7 +290,7 @@
             Console.WriteLine("=======================================");
         }
     
-        static char[,] updateMyMap(char[,] myMatrix)
+        static char[,] updateMyMapCharMovement(char[,] myMatrix)
         {
             myMatrix[playerPosY, playerPosX] = ' ';
 
@@ -373,30 +302,3 @@
     
     }
 
-
-
-
-
-namespace StudioA
-{
-
-    internal class Calculator
-    {
-        // Προσθέστε τη λέξη static για να μπορεί να κληθεί άμεσα
-        public static void Message()
-        {
-            Console.WriteLine("This class belongs to namespace StudioA.");
-        }
-    }
-}
-namespace StudioB
-{
-    class Calculator
-    {
-        // Προσθέστε τη λέξη static εδώ επίσης
-        public static void Message()
-        {
-            Console.WriteLine("This class belongs to namespace StudioB.");
-        }
-    }
-}
